74cf18fce808bf246ba757b72890afd2cbea4d5a
FinlayTheBerry <randomiagaming@gmail.com>
2025-12-27 03:02:06 -0800

    Bump removed useless dep from sleepmute.nix and renamed mutealsa to sleepmute in .gitignore.

--- sleepmute.nix
+++ sleepmute.nix
@@ -1,8 +1,8 @@
 { config, pkgs, ... }:
 let
   sleepmute-bin = pkgs.fetchurl {
-    url = "https://github.com/FinlayTheBerry/sleepmute/releases/download/v0.1.1/sleepmute_static";
-    hash = "sha256-Y2zO6YO2PUE3bm/m6lV7hpJGBQadB3USV5YdJPCSu1E="; 
+    url = "https://github.com/FinlayTheBerry/sleepmute/releases/download/v0.2.0/sleepmute_static";
+    hash = "sha256-Y2zO6YO2PUE3bm/m6lV7hpJGBQadB3USV5YdJPCSu1E=";
     executable = true;
   };
 in
@@ -10,18 +10,18 @@ in
   systemd.services.sleepmute-service = {
     description = "Runs sleepmute on sleep/hibernate.";
     wantedBy = [ 
-      "sleep.target" 
-      "suspend.target" 
-      "hibernate.target" 
-      "hybrid-sleep.target" 
-      "suspend-then-hibernate.target" 
+      "sleep.target"
+      "suspend.target"
+      "hibernate.target"
+      "hybrid-sleep.target"
+      "suspend-then-hibernate.target"
     ];
-    before = [ 
-      "sleep.target" 
-      "suspend.target" 
-      "hibernate.target" 
-      "hybrid-sleep.target" 
-      "suspend-then-hibernate.target" 
+    before = [
+      "sleep.target"
+      "suspend.target"
+      "hibernate.target"
+      "hybrid-sleep.target"
+      "suspend-then-hibernate.target"
     ];
     unitConfig = {
       StopWhenUnneeded = "yes";
@@ -29,7 +29,6 @@ in
     serviceConfig = {
       Type = "oneshot";
       RemainAfterExit = "yes";
-      Environment = "ALSA_CONFIG_PATH=${pkgs.alsa-lib}/share/alsa/alsa.conf";
       ExecStart = "${sleepmute-bin} pre";
       ExecStop = "${sleepmute-bin} post";
       User = "root";
